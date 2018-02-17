aa7d64db3bb31904efbff14e5445be010f29648c
Salvador Delgado <sdelgado84@gmail.com>
2018-02-17 15:07:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,14 +4,3 @@ def valid_move?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def turn(board)
-  puts "Please enter 1-9:"
-  UserInput = gets.strip
-  index = input_to_index (userInput)
-  if !valid_move?(board, index)
-    turn(board)
-  else
-    move(board, index, current_player(board))
-  end
-  display_board(board)
-end
